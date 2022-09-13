f6b28d6c9dc577cdc89838c530b7d83780bde80b
bitbucket_pooh <punyapat.kha@gmail.com>
2022-09-13 21:37:57 +0700

    test

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_Q1TBSqglmsXpR40S_pzr-HlftfKAB1NRdZKhqNTR2rAnv8lbavH74KNIOUsT3yxj8hBUwTsmaKpp/pubhtml?gid=1189134930&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+
+<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_Q1TBSqglmsXpR40S_pzr-HlftfKAB1NRdZKhqNTR2rAnv8lbavH74KNIOUsT3yxj8hBUwTsmaKpp/pubhtml?gid=1908802155&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+<div>________________________________________________________________________</div>
+<div>
+<a href="https://docs.google.com/forms/d/e/1FAIpQLSfkEk1cxMavN5FfvmLkuBKqyL6v41nRpNXr3AK8dMMRI6T9sA/viewform">แจ้งทะเบียนรถหาย</a>
+</div>
+<div>
+<a href="https://docs.google.com/forms/d/e/1FAIpQLSd1tAO40fF88S0EzDNdxpsi6elpPhWGdF26QKm_XNqiXU5O7Q/viewform">แจ้งพบทะเบียนรถ</a>
+</div>
+<div>________________________________________________________________________</div>
+
+<div>
+หากเลขทะเบียนที่ถูกแจ้งหายและแจ้งพบ Match กัน
+ระบบจะส่งข้อมูลของผู้พบ ให้ผู้ที่แจ้งหายทาง Email
+</div>
+<div>________________________________________________________________________</div>
+<div>
+ข้อมูลสำหรับการติดต่อจะไม่ถูกเผยแพร่สู่สาธารณะ
+และไม่ถูกนำไปใช้เชิงพาณิชย์
+หรือผลประโยชน์อื่นใด นอกเหนือจากการรับ-ส่งมอบทะเบียนรถคืนเท่านั้น
+</div>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
+  // ,components: {
+    // HelloWorld
+  // }
 }
 </script>
 
